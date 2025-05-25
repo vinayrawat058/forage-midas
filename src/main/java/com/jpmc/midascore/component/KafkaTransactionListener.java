a5385dfdb38b8315348725471e28437b64feb44d
vinayrawat058 <vinay.rawat058@gmail.com>
2025-05-24 23:58:58 -0400

    Added H2 integration

--- src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
@@ -1,14 +1,21 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.service.TransactionService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaTransactionListener {
-    @KafkaListener(topics = "${general.kafka-topic}")
+    private final TransactionService transactionService;
+
+    public KafkaTransactionListener(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @KafkaListener(topics = "${general.kafka-topic}" , groupId = "midas-core-group")
     public void receiveMessage(Transaction transaction) {
-    System.out.println("Received transaction: " + transaction);
+        transactionService.processTransaction(transaction);
+    }
 }
 
-}
